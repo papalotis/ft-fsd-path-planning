3c686e17e87c541f051f68b921cc461b30de40c0
Panagiotis Karagiannis <papalotis1@gmail.com>
2026-06-14 17:47:12 +0200

    fix(lint): apply ruff autofix for line length issues in streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -38,7 +38,7 @@ The path planning algorithm is split into three parts:
 There is a special page for visualizing the algorithms that run when the Skidpad mission is selected.
 
 At the top of the page you can select the algorithm you want to explore.
-""".strip()
+""".strip()  # noqa: E501
     )
 
 
@@ -77,15 +77,13 @@ with st.sidebar:
     if st.session_state.track_configuration == "Custom":
         json_text = st.text_area(
             "JSON Input",
-            help='Put a string that represents a JSON dict here. The dict should have the keys "vehicle_position", "vehicle_direction", "cones_left" and "cones_right". The vehicle position and direction should be a list of two numbers, the cones should be a list of lists of two numbers.',
+            help='Put a string that represents a JSON dict here. The dict should have the keys "vehicle_position", "vehicle_direction", "cones_left" and "cones_right". The vehicle position and direction should be a list of two numbers, the cones should be a list of lists of two numbers.',  # noqa: E501
         ).strip()
         st.session_state.json_text = json_text
 
 if st.session_state.track_configuration == "Custom":
     st.warning(
-        "You can input arbitrary track configurations here. The path planner has not been thoroughly"
-        " tested so it is possible that it will not work for all configurations. It is not difficult"
-        " to find edge cases that will break the algorithm."
+        "You can input arbitrary track configurations here. The path planner has not been thoroughly tested so it is possible that it will not work for all configurations. It is not difficult to find edge cases that will break the algorithm."  # noqa: E501
     )
 
 
